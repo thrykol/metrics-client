528d12b51d440feb8897f1e3f0e4f3c875edda96
thrykol <thrykol@gmail.com>
2017-05-26 15:01:33 -0600

    Added converters for metrics and metric formats

--- client/src/main/scala/us/my_family/metrics/implicits/Converters.scala
+++ client/src/main/scala/us/my_family/metrics/implicits/Converters.scala
@@ -1,8 +1,41 @@
 package us.my_family.metrics.implicits
 
-import us.my_family.metrics.TagMap
+import com.typesafe.scalalogging.LazyLogging
 
-object Converters {
+import us.my_family.metrics.Metric
+import us.my_family.metrics.TagMap
+import us.my_family.metrics.configuration.Configuration
+import us.my_family.metrics.configuration.ConfigurationProvider
+import us.my_family.metrics.configuration.DogStatsDFormat
+import us.my_family.metrics.configuration.MetricFormat
+import us.my_family.metrics.configuration.StatsDFormat
+import us.my_family.metrics.configuration.TelegrafFormat
+import us.my_family.metrics.writers.DogStatsDWriter
+import us.my_family.metrics.writers.StatsDWriter
+import us.my_family.metrics.writers.TelegrafWriter
+
+object Converters extends Converters
+
+trait Converters extends ConfigurationProvider with LazyLogging {
 
 	implicit def optionMap(data : Map[String, String]) : TagMap = data map { case (k, v) => (k -> Option(v)) }
+
+	implicit def metricToString(metric : Metric) = {
+
+		Configuration.format match {
+			case StatsDFormat => new Metric(metric.metric, metric.metricType, metric.value, metric.tags, metric.sampleRate) with StatsDWriter write
+			case DogStatsDFormat => new Metric(metric.metric, metric.metricType, metric.value, metric.tags, metric.sampleRate) with DogStatsDWriter write
+			case TelegrafFormat => new Metric(metric.metric, metric.metricType, metric.value, metric.tags, metric.sampleRate) with TelegrafWriter write
+		}
+	}
+
+	implicit def stringToMetricFormat(format : String) : MetricFormat = format.toLowerCase match {
+		case "dogstatsd" => DogStatsDFormat
+		case "telegraf" => TelegrafFormat
+		case "statsd" => StatsDFormat
+		case other => {
+			logger.warn(s"Unknown metric format '${other}': defaulting to StatsD")
+			StatsDFormat
+		}
+	}
 }
--- client/src/test/scala/us/my_family/metrics/implicits/ConvertersSpec.scala
+++ client/src/test/scala/us/my_family/metrics/implicits/ConvertersSpec.scala
@@ -2,8 +2,28 @@ package us.my_family.metrics.implicits
 
 import org.scalatest.Matchers
 import org.scalatest.WordSpec
+import org.scalatest.prop.TableDrivenPropertyChecks
+
+import us.my_family.metrics.Counter
+import us.my_family.metrics.Metric
+import us.my_family.metrics.configuration.DogStatsDFormat
+import us.my_family.metrics.configuration.StatsDFormat
+import us.my_family.metrics.configuration.TelegrafFormat
+import us.my_family.metrics.test.ConfigurationFixture
+import us.my_family.metrics.test.LoggingFixture
+import us.my_family.metrics.writers.DogStatsDWriter
+import us.my_family.metrics.writers.StatsDWriter
+import us.my_family.metrics.writers.TelegrafWriter
 
 class ConvertersSpec extends WordSpec with Matchers {
+
+	trait BaseFixture extends ConfigurationFixture with LoggingFixture {
+		lazy val metric = Metric("metric.name", Counter, "metric.value", Map(), 1.0)
+
+		lazy val converters = new Converters {
+			override protected lazy val logger = BaseFixture.this.logger
+		}
+	}
 
 	"optionMap" should {
 
@@ -13,6 +33,56 @@ class ConvertersSpec extends WordSpec with Matchers {
 			val expected = Map("defined" -> Option("defined"), "null" -> None)
 
 			Converters.optionMap(data) shouldBe expected
+		}
+	}
+
+	"metricToString" should {
+
+		"convert to StatsDFormat" in new BaseFixture {
+
+			val statsd = new Metric(metric.metric, metric.metricType, metric.value, metric.tags, metric.sampleRate) with StatsDWriter
+
+			Converters.metricToString(statsd) shouldBe statsd.write()
+		}
+
+		"convert to DogStatsDFormat" in new BaseFixture {
+
+			val dogstatsd = new Metric(metric.metric, metric.metricType, metric.value, metric.tags, metric.sampleRate) with DogStatsDWriter
+
+			Converters.metricToString(dogstatsd) shouldBe dogstatsd.write()
+		}
+
+		"convert to TelegrafFormat" in new BaseFixture {
+
+			val telegraf = new Metric(metric.metric, metric.metricType, metric.value, metric.tags, metric.sampleRate) with TelegrafWriter
+
+			Converters.metricToString(telegraf) shouldBe telegraf.write()
+		}
+	}
+
+	"stringToMetricFormat" should {
+
+		"convert the expected strings" in new BaseFixture with TableDrivenPropertyChecks {
+
+			forAll(Table(("text", "format"),
+				("statsd", StatsDFormat),
+				("dogstatsd", DogStatsDFormat),
+				("telegraf", TelegrafFormat))) { (text, format) =>
+				converters.stringToMetricFormat(text) shouldBe format
+				converters.stringToMetricFormat(text.toUpperCase()) shouldBe format
+				converters.stringToMetricFormat(text.capitalize) shouldBe format
+			}
+		}
+
+		"treat unknown strings as StatsD" in new BaseFixture {
+
+			override lazy val isWarnEnabled = true
+
+			lazy val other = "bad-wolf"
+
+			(underlying.warn(_ : String)) expects (s"Unknown metric format '${other}': defaulting to StatsD")
+
+			converters.stringToMetricFormat(other) shouldBe StatsDFormat
 		}
 	}
 }
